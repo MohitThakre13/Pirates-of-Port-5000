fb9684efcf9b29eeedb0a49e6474bd3ea9fd2856
Shweta Kesharwani <shwetak>
2023-07-12 01:42:56 +0530

    advice-page

--- header.php
+++ header.php
@@ -26,6 +26,21 @@
         <div class="mid-nav">
             <ul class="navopt">
                 <li class="on-page"><a href="./homepage.php" id="home">Home</a></li>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Blinker:wght@300&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+
+<body>
+    <nav class="header">
+        <div class="logo" href="#">
+            <img src="./Images/openLakeLogo.png" alt="">
+            <h3>Openlake.</h3>
+        </div>
+
+        <div class="mid-nav">
+            <ul class="navopt">
+                <li class="on-page"><a href="#" id="home">Home</a></li>
                 <li><a href="#" id="our-projects">Our Projects</a></li>
                 <li><a href="#" id="community">Community</a></li>
                 <li><a href="#" id="mentors">Mentors</a></li>
@@ -48,6 +63,19 @@
             </div>
         </a>
 
+
+        <div class="right-nav">
+            <button id="blog-btn" href="#">Blogs</button>
+            <button id="subscribe-btn" href="#">Subscribe</button>
+        </div>
+    </nav>
+    <nav class="header-mobile">
+  
+        <div class="logo" href="#">
+            <img src="./Images/openLakeLogo.png" alt="">
+            <h3>Openlake.</h3>
+        </div>
+  
         <input class="side-menu" type="checkbox" id="side-menu" />
         <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
 
@@ -59,6 +87,12 @@
                 <li><a href="#">Mentors</a></li>
                 <li><a href="./blogs.php">Blogs</a></li>
                 <li><a href="#subscribe">Subscribe</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Our Projects</a> </li>
+                <li><a href="#">Community</a> </li>
+                <li><a href="#">Mentors</a></li>
+                <li><a href="#">Blogs</a></li>
+                <li><a href="#">Subscribe</a></li>
             </ul>
         </nav>
     </nav>
