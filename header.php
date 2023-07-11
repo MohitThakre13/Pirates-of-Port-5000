cafaaeae1d8906d559f90fcf5ba2d9f1fe45c993
piyush-c38 <chandrakarpiyush36@gmail.com>
2023-07-11 19:39:16 +0530

    deleting try, home and changing header.php

--- header.php
+++ header.php
@@ -7,50 +7,58 @@
     <title>Home</title>
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Blinker:wght@300&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&family=Chakra+Petch:wght@300&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-
-<body>
+    
+    <body>
     <nav class="header">
-        <div class="logo" href="#">
-            <img src="./Images/openLakeLogo.png" alt="">
-            <h3>Openlake.</h3>
-        </div>
-
+        <a href="./homepage.php">
+            <div class="logo">
+                <img src="./Images/openLakeLogo.png" alt="">
+                <h3>Openlake.</h3>
+            </div>
+        </a>
         <div class="mid-nav">
             <ul class="navopt">
-                <li class="on-page"><a href="#" id="home">Home</a></li>
+                <li class="on-page"><a href="./homepage.php" id="home">Home</a></li>
                 <li><a href="#" id="our-projects">Our Projects</a></li>
                 <li><a href="#" id="community">Community</a></li>
                 <li><a href="#" id="mentors">Mentors</a></li>
             </ul>
         </div>
-
         <div class="right-nav">
-            <button id="blog-btn" href="#">Blogs</button>
-            <button id="subscribe-btn" href="#">Subscribe</button>
+            <ul>
+                <li><a href="./blogs.php" id="blog-btn">Blogs</a></li>
+                <li><a href="#subscribe" id="subscribe-btn">Subscribe</a></li>
+            </ul>
         </div>
     </nav>
+
     <nav class="header-mobile">
-  
-        <div class="logo" href="#">
-            <img src="./Images/openLakeLogo.png" alt="">
-            <h3>Openlake.</h3>
-        </div>
-  
+
+        <a href="./homepage.php">
+            <div class="logo">
+                <img src="./Images/openLakeLogo.png" alt="">
+                <h3>Openlake.</h3>
+            </div>
+        </a>
+
         <input class="side-menu" type="checkbox" id="side-menu" />
         <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
 
         <nav class="nav">
             <ul class="menu">
-                <li><a href="#">Home</a></li>
+                <li><a href="./homepage.php">Home</a></li>
                 <li><a href="#">Our Projects</a> </li>
                 <li><a href="#">Community</a> </li>
                 <li><a href="#">Mentors</a></li>
-                <li><a href="#">Blogs</a></li>
-                <li><a href="#">Subscribe</a></li>
+                <li><a href="./blogs.php">Blogs</a></li>
+                <li><a href="#subscribe">Subscribe</a></li>
             </ul>
         </nav>
     </nav>
