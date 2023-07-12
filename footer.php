e570b502945d4ff4a13b40be365fc163d166373d
piyush-c38 <chandrakarpiyush36@gmail.com>
2023-07-12 16:06:14 +0530

    making footer responsive

--- footer.php
+++ footer.php
@@ -1,27 +1,25 @@
 <footer class="footer">
 
     <link rel="stylesheet" href="footer_styling.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
 
     
-    <div>
-        <div class="row">
-            <div class= "column1 column1">
+
+        <div class="container">
+            <div class= "column1">
                 <p class="copyright"><a href="https://www.instagram.com/openlake_iitbhilai" class="footer__link footer__link--instagram"><i class="fab fa-instagram"></i></a> &nbsp; <a href="https://in.linkedin.com/company/openlake" class="footer__link footer__link--linkedin"><i class="fab fa-linkedin"></i></a> &nbsp; <a href="mailto:openlake@iitbhilai.ac.in" class="footer__link footer__link--webmail"><i class="fas fa-envelope"></i></a></p>
             </div>
-                
-            
-
-            <div class="column2 column2"><p class="love"> &copy; OpenLake, IIT Bhilai</p></div>
+            <div class="column2">
+                <p class="spec"> &copy; OpenLake, IIT Bhilai</p>
+            </div>
         </div>
-    </div>
+ 
 
 </footer>
 
-<script
-src="https://code.jquery.com/jquery-2.2.4.min.js"
-integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-crossorigin="anonymous"></script>
+<script>
+    src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous">
+</script>
 
 <script src="js/scrollspy.js"></script>
 <script src="js/script.js"></script>
