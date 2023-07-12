4cd1ec13a7e9eb23ca551eb5f7485e38398b8552
Shweta Kesharwani <shwetak>
2023-07-12 10:03:16 +0530

    advice-page2

---
+++ advice.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="advice.css">
+    <link rel="stylesheet" type="text/css" href="footer_styling.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+        integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <title>Mentor's Insights</title>
+</head>
+
+<body>
+
+
+    <?php
+    require_once('header.php');
+    ?>
+
+    <div class="background1">
+      Mentor's Insights
+    </div>
+    <div class="background2">
+        <br><br>
+        <p class="head5"><b>MEET THE MENTORS</b></p>
+        <br><br><>
+
+
+        <div class="shift-right"><div class="container1">
+            <div><img src="images/m1.jpg" alt="A beautiful image" width="300" height="200"
+                    style="border :1px solid #fff" class="rounded-image"><a href="https://www.instagram.com/iitbhilai/"
+                    class="footer__link footer__link--instagram"><i class="fab fa-instagram"></i>
+                    <a href="https://www.linkedin.com/school/iit-bhilai/" class="footer__link footer__link--linkedin"><i
+                            class="fab fa-linkedin"></i>
+            </div>
+
+            <div>
+                <div class="image-container1">
+                    <img src="images/box.jpg" alt="A beautiful image" width="700" height="200"
+                        style="border: 1px solid #fff" class="rounded-image">
+                    <div class="text-overlay">
+
+
+                        <p class="advice">We also don't always experience pure forms of each
+                            emotion. Mixed emotions over different events or
+                            situations in our lives are common. When faced with starting a new
+                            job, you might feel both excited
+                            and nervous. Getting married or having a child might be marked by a
+                            wide variety of emotions ranging
+                            from joy to anxiety. These emotions might occur simultaneously, or
+                            you might feel them one after
+                            another.<br><br>
+                            Anshu Ghosh, Btech 2024
+                        </p>
+                    </div>
+                </div>
+            </div>
+</div>
+        </div>
+
+
+
+
+        <br><br><br>
+
+
+        <div>
+            <div class="container1">
+                <div><img src="images/m1.jpg" alt="A beautiful image" width="300" height="200"
+                        style="border :1px solid #fff" class="rounded-image"><a
+                        href="https://www.instagram.com/iitbhilai/" class="footer__link footer__link--instagram"><i
+                            class="fab fa-instagram"></i>
+                        <a href="https://www.linkedin.com/school/iit-bhilai/"
+                            class="footer__link footer__link--linkedin"><i class="fab fa-linkedin"></i></div>
+
+                <div>
+                    <div class="image-container1">
+                        <img src="images/box.jpg" alt="A beautiful image" width="700" height="200"
+                            style="border: 1px solid #fff" class="rounded-image">
+                        <div class="text-overlay">
+
+
+                            <p class="advice">We also don't always experience pure forms of each
+                                emotion. Mixed emotions over different events or
+                                situations in our lives are common. When faced with starting a new
+                                job, you might feel both excited
+                                and nervous. Getting married or having a child might be marked by a
+                                wide variety of emotions ranging
+                                from joy to anxiety. These emotions might occur simultaneously, or
+                                you might feel them one after
+                                another.<br><br>
+                                Anshu Ghosh, Btech 2024
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br><br><br>
+
+
+            <div>
+            <div class="container1">
+                <div><img src="images/m1.jpg" alt="A beautiful image" width="300" height="200"
+                        style="border :1px solid #fff" class="rounded-image"><a
+                        href="https://www.instagram.com/iitbhilai/" class="footer__link footer__link--instagram"><i
+                            class="fab fa-instagram"></i>
+                        <a href="https://www.linkedin.com/school/iit-bhilai/"
+                            class="footer__link footer__link--linkedin"><i class="fab fa-linkedin"></i></div>
+
+                <div>
+                    <div class="image-container1">
+                        <img src="images/box.jpg" alt="A beautiful image" width="700" height="200"
+                            style="border: 1px solid #fff" class="rounded-image">
+                        <div class="text-overlay">
+
+
+                            <p class="advice">We also don't always experience pure forms of each
+                                emotion. Mixed emotions over different events or
+                                situations in our lives are common. When faced with starting a new
+                                job, you might feel both excited
+                                and nervous. Getting married or having a child might be marked by a
+                                wide variety of emotions ranging
+                                from joy to anxiety. These emotions might occur simultaneously, or
+                                you might feel them one after
+                                another.<br><br>
+                                Anshu Ghosh, Btech 2024
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br><br><br>
+
+
+
+
+
+
+
+
+            <br><br><br>
+
+            <br><br><br>
+
+
+            <?php
+            require_once('footer.php');
+            ?>
+
+
+        </div>
+</body>
+
+</html>
