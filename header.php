7b61672a57e7e1c3ba66c084b8cef54a103412b7
vedanshbvb <vedanshs@iitbhilai.ac.in>
2023-07-14 19:42:08 +0530

    linked final projects

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
         <div class="mid-nav">
             <ul class="navopt">
                 <li class="on-page-home"><a href="./homepage.php" id="home">Home</a></li>
-                <li class="on-page-project"><a href="./project.php" id="our-projects">Our Projects</a></li>
+                <li class="on-page-project"><a href="./finalprojects.php" id="our-projects">Our Projects</a></li>
                 <li class="on-page-community"><a href="#" id="community">Community</a></li>
                 <li class="on-page-advice"><a href="./advice.php" id="advice">Advice</a></li>
             </ul>
