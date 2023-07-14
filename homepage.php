c0f4cd39ddebac9a1df09936e4b765d0d54f9f26
Mohit Thakre <mohitt@iitbhilai.ac.in>
2023-07-15 00:01:25 +0530

    k

--- homepage.php
+++ homepage.php
@@ -14,17 +14,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="./homepage.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
     
 </head>
 <body>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
     <?php
       require_once('header.php');
     ?>
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-<?php
+    <?php
         if (isset($_POST['submit'])) {
             if (strlen($_POST['box']) != 0) {
                 $email = $_POST['box'];
@@ -55,11 +54,10 @@
                     die("There is a technical issue : ".mysqli_connect_error());
                 }
             }
+        
+
         }
-        ?>
-!
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+?>
 
     <img src="./Images/arrow.png" alt="" id="arrow">
     <section class="main-intro-container">
